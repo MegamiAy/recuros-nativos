09246660d4c3bfa9a8b12b84caa724cfb3ea1f87
Megami <megami.ay@gmail.com>
2023-04-24 14:31:44 -0300

    a

--- screens/DeviceInfo.jsx
+++ screens/DeviceInfo.jsx
@@ -1,9 +1,50 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import * as Device from 'expo-device';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+        fontSize: 35,
+        backgroundColor: '#939',
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 10,
+        padding: 10,
+    },
+
+    info: {
+        fontSize: 14,
+        backgroundColor: '#5e4d85',
+        color: '#fff',
+        marginBottom: 10,
+        padding: 10,
+    }
+  });
 
 export default function DeviceInfo(){
     return(
         <View>
-            <Text>Device Info</Text>
+            <Text style={styles.title}>INFORMAÇÕES DO APARELHO</Text>
+            <Text style={styles.info}>
+                O nome do dispositivo: {Device.deviceName}
+            </Text>
+            <Text style={styles.info}>
+                Seu dispositivo é: {Device.brand}
+            </Text>
+            <Text style={styles.info}>
+                O modelo do seu dispositivo: {Device.modelName} e o modeloId: {Device.modelId}
+            </Text>
+            <Text style={styles.info}>
+                A versão do dispositovo: {Device.osVersion}
+            </Text>
+            <Text style={styles.info}>
+                A memória total do dispositivo: {Device.totalMemory}
+            </Text>
         </View>
     )
 }
